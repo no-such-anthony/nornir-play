3c73420a2237535dc9194505a11709eefa2b96bb
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2019-09-28 10:05:29 +1000

    Update nrlogin.py

--- nrlogin.py
+++ nrlogin.py
@@ -1,9 +1,14 @@
-import subprocess
-import sys
 from nornir import InitNornir
+import sys
+import interactive
 
 #Interactive ssh after automatic login with nornir credentials
 
+def netmiko_interactive(task):
+    net_connect = task.host.get_connection("netmiko", task.nornir.config)
+    print('Press enter to start!')
+    interactive.interactive_shell(net_connect.remote_conn)
+    
 if len(sys.argv) == 2:
     hostname = sys.argv[1]
 else:
@@ -11,10 +16,6 @@ else:
     sys.exit(1)
 
 nr = InitNornir('config.yaml')
-host = nr.inventory.hosts[hostname]
+nr = nr.filter(name=hostname)
 
-hostname = host.hostname
-username = host.username
-password = host.password
-
-status = subprocess.call("sshpass" + f" -p {password} ssh -l {username} {hostname}", shell=True)
+results = nr.run(task=netmiko_interactive)
