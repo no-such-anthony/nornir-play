2141278350da70e6ed52e70dff42e4a435e10090
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2019-10-05 17:41:01 +1000

    Update custom_exception.py

--- custom_exception.py
+++ custom_exception.py
@@ -5,7 +5,7 @@ from nornir.core.exceptions import NornirSubTaskError
 
 #only an example, obvisously these are not actual errors.
 
-#temporary fix for nornir <=2.3 "logging: {'enabled': False}'" 
+#temporary fix for nornir <=2.3 "logging={'enabled': False}" 
 #nornir skips logger config when set to false and ends up 
 #with default logger settings, eg level NOTSET(0), etc
 #when logger.error('') runs within Nornir Task exception it prints 
