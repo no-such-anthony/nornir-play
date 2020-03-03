489757aca095ae4ce284ce7167f28ec519e325bf
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2020-03-03 21:56:20 +1000

    Update gather-configs.py

--- gather-configs.py
+++ gather-configs.py
@@ -7,7 +7,7 @@ from netmiko import NetMikoAuthenticationException, NetMikoTimeoutException
 import time
 from datetime import datetime
 import re
-#from creds import insert_creds
+from creds import insert_creds
 
 
 BACKUPDIR = '../configs'
@@ -38,7 +38,7 @@ def config_filter_cisco_ios(cfg):
     return cfg
 
 
-def backup(task, path):
+def backup(task):
   host = task.host
 
   retries = 0
@@ -79,7 +79,24 @@ def backup(task, path):
             if retries == 2:
               raise e         
           else:
-            raise e              
+            raise e  
+
+        if isinstance(e.result.exception, OSError):
+          if 'Search pattern' in e.result[0].result:
+            print(f'{host} hit error {e.result.exception}')
+            if retries == 2:
+              raise e         
+          else:
+            raise e
+
+        if isinstance(e.result.exception, EOFError):
+          if 'closed by remote device' in e.result[0].result:
+            print(f'{host} hit error {e.result.exception}')
+            if retries == 2:
+              raise e         
+          else:
+            raise e
+
         task.results.pop()
         time.sleep(10)
 
@@ -95,6 +112,7 @@ def backup(task, path):
 
     retries += 1
 
+  host.close_connections()
   data.result = config_filter_cisco_ios(data.result)
 
   #occassionly an empty line appears in the middle of IOS config not sure why
@@ -103,19 +121,19 @@ def backup(task, path):
     data.result = re.sub(r'^\s*$', "", data.result, flags=re.M)
 
   task.run(write_file,
-           filename=f'{path}/{host}.cfg',
+           filename=f'{BACKUPDIR}/{host}.cfg',
            content=data.result)
  
 
 def main():
   nr = InitNornir(config_file='../config.yaml',
-                  core={'num_workers': 20},
+                  core={'num_workers': 100},
                   )
-  #insert_creds(nr.inventory)
+  insert_creds(nr.inventory)
   ios_filt = ~F(platform="cisco_wlc")
   ios = nr.filter(ios_filt)
   start_time = datetime.now()
-  result = ios.run(task=backup, path=BACKUPDIR)
+  result = ios.run(task=backup)
   elapsed_time = datetime.now() - start_time
 
   print('-'*50)
@@ -147,15 +165,22 @@ Issues.  With 20 workers on 1300 devices I experienced the following random erro
 3.Corrupt backups < 100 characters
 4.NetMikoAuthenticationException, primarily on cisco_nxos as they as they don't like tacacs+ timeouts, 
   and may have fallen back to local
+Plus others if we increase workers.
 
-Fixes
-- Use Netmiko 3.0, hopefully you should no longer need while/try/except block.
+Possible Fixes?
+- Use Netmiko 3.0
 or 
 - keep reducing num_workers
 or
 - two retries, 
   - catch errors and retry
   - check if len(data.result) < 100 and retry
+or
+- Look into your AAA server
+or
+- Close connections within task
+or
+- use global delay factor of 2
 
 Diff Issues
 A lot of Cisco devices will show these in the diffs...hence the filter before write_file
